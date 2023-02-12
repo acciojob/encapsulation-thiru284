41b7990f6cd967c0e8f11539bb2a00f93288b9fe
Thirumoorthy Velumani <thirumoorthy@mallow-tech.com>
2023-02-12 18:09:43 +0530

    Soln

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
-package com.driver;
+import com.driver.RWOnly;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+    obj.setName("hello");
+    obj.getName();
+  }
 }
